da57498a16a4b76332f2538a26b47333093a924e
sameershahid1 <goku.hulk234@gmail.com>
2024-06-27 02:45:07 +0500

    remove line on main

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module vetner360-backend
 
-go 1.21.5
+go 1.22.4
+// go 1.21.5
 
 require (
 	github.com/go-chi/chi/v5 v5.0.12
--- main.go
+++ main.go
@@ -13,21 +13,19 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("Error loading .env file:", err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	fmt.Println("Error loading .env file:", err)
+	// }
 
 	url := os.Getenv("MONGODB_URI")
 	port := os.Getenv("PORT")
 	if port != "" {
 		port = "8080"
 	}
-
 	database.ConnectWithMongoDB(url)
 
 	// for collectionName, columnList := range static_data.IndexCollection {
